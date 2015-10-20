20d8aca575b05ebc6fbd54a6d1e1e63279a4bcfa
mikey1977 <mmi@umich.edu>
2015-10-20 10:41:02 -1000

    minifier working, removing spaces and line breaks

--- minify.js
+++ minify.js
@@ -1,26 +1,70 @@
+// var fs = require('fs');
+// var util = require('util');
+// var Transform = require('stream').Transform;
+// var inherits = require('util').inherits;
+
+
+// //create array from terminal input and slice off first two properties
+// var inputArray = require('minimist')(process.argv.slice(2));
+
+
+// // read from file app.css
+// var readableStdin = fs.createReadStream(inputArray.input);
+
+// // fs.writeFile(inputArray.output);
+
+// //create app.min.css file
+// var writeableStdout = fs.createWriteStream(inputArray.output);
+
+// //constructor
+// function Transformer() {
+//   Transform.call(this);
+// }
+
+// inherits(Transformer, Transform);
+
+// Transformer.prototype._transform = function(chunk, encoding, done) {
+//   chunk = chunk.toString().replace(/(\r\n|\n|\r|\s)/gm, "");
+//   this.push(chunk);
+//   done();
+// };
+
+
+// readableStdin.pipe(new Transformer).pipe(writeableStdout);
+
+
+//===========================================
+
 var fs = require('fs');
 var util = require('util');
-var stream = require('stream');
+var Transform = require('stream').Transform;
+var inherits = require('util').inherits;
 
-//create array from terminal input
-var inputArray = process.argv[2];
+
+//create array from terminal input and slice off first two properties
+// var inputArray = require('minimist')(process.argv.slice(2));
+
 
 // read from file app.css
 var readableStdin = fs.createReadStream('app.css');
 
+// fs.writeFile(inputArray.output);
+
 //create app.min.css file
 var writeableStdout = fs.createWriteStream('app.min.css');
 
-var transformInput;
+//constructor
+function Transformer() {
+  Transform.call(this);
+}
+
+inherits(Transformer, Transform);
+
+Transformer.prototype._transform = function(chunk, encoding, done) {
+  chunk = chunk.toString().replace(/(\r\n|\n|\r|\s)/gm, "");
+  this.push(chunk);
+  done();
+};
 
 
-readableStdin.on('data', function(chunk) {
-
-
-  writeableStdout.write()
-})
-//read - in
-
-//transform stream - removing line breaks
-
-//write - out minified file
+readableStdin.pipe(new Transformer).pipe(writeableStdout);
